c5bce317c999a4f5fa3aa7172e789ee1d78aa761
Grace Olafare <oluwafunhmi@gmail.com>
2023-08-29 06:58:58 +0000

    Adding api endpoint Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,22 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api", (req, res) => {
+    const now = new Date();
+    res.json({ unix: now.getTime(), utc: now.toUTCString() })
+});
 
+app.get("/api/:date", (req, res) => {
+    const paramsDate = req.params.date;
+    const invalidDate = "Invalid Date";
+    const date = parseInt(paramsDate) < 10000
+        ? new Date(paramsDate)
+        : new Date(parseInt(paramsDate))
+
+    date.toString() === invalidDate
+        ? res.json({ error: invalidDate })
+        : res.json({ unix: date.valueOf(), utc: date.toUTCString() });
+});
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
